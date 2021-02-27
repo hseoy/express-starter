c1952cb8338a77fd6ee35202248e691bb09771a5
Yunseo Hwang <yunseo.dev@gmail.com>
2021-02-27 20:33:11 +0900

    Use morgan package for logging
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.json('pick for me api');
+app.get('/', (_req, res) => {
+  res.json('express-starter api');
 });
 
 const { PORT } = process.env;
